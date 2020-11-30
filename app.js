5f83e601fc3e07d64ecd0e3400b64d1b7a8e8fc7
Sachin Pant <sachinpant96@gmail.com>
2020-11-30 19:56:29 +0530

    Banana App done

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textArea = document.querySelector("#text-area");
+var outputDiv = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time!");
+}
 
 function clickHandler() {
-  console.log("clicked!");
-  console.log("input", textArea.value);
+  //   outputDiv.innerText = "asjsjsj " + textArea.value;
+  var textInput = textArea.Value; //taking input
+
+  // Calling server for processing
+  fetch(getTranslationURL(textInput))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
